53812b667ca5caeb85c4fa1257966e40b3babe82
Mariana Maldonado <mmaldonado9@ucol.mx>
2020-05-07 14:24:55 -0500

    Add splash screen component

--- App.js
+++ App.js
@@ -5,11 +5,8 @@ import SplashScreen from 'react-native-splash-screen'
 export default class App extends Component {
 
   componentDidMount() {
-    // do stuff while splash screen is shown
-      // After having done stuff (such as async tasks) hide the splash screen
       SplashScreen.hide();
   }
-
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
